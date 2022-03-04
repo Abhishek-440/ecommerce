b4cff61be711308740b8454571ecb528cc93c99b
Abhishek-440 <abhishek.shrestha@gurzu.net>
2022-03-04 10:32:05 +0545

    validation added and working on axois post

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Form, Button, Row, Col, Container } from "react-bootstrap";
-//import * as Yup from "yup";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
-function Register() {
+const schema = yup.object({
+  username: yup.string().required("This field is required"),
+  address: yup.string().required("This field is required"),
+  email: yup.string().email().required("This field is required"),
+  number: yup.number().required("This field is required"),
+  dob: yup.date().min(new Date(1980, 0, 1)).required("This Field is required."),
+  password: yup
+    .string()
+    .required("Password is mendatory")
+    .min(3, "Password must be at 3 char long"),
+  confirmPwd: yup
+    .string()
+    .required("Password is mendatory")
+    .oneOf([yup.ref("password")], "Passwords does not match"),
+});
+
+const Register = ({ value }) => {
+  const [data, setData] = useState({
+    username: "",
+    address: "",
+    email: "",
+    number: "",
+    dob: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setData({
+      ...data,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      username: data.username,
+      address: data.address,
+      email: data.email,
+      number: data.number,
+      dob: data.dob,
+      password: data.password,
+    };
+    axios
+      .post("https://localhost:3005/api/register", userData)
+      .then((response) => {
+        console.log(response.status);
+        console.log(response.data.token);
+      });
+  };
+  const { onSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  //const onSubmit = (value) => console.log(value);
+
   return (
     <>
       <Container className="p-5 mw-60">
@@ -11,23 +72,42 @@ function Register() {
             Register
           </h1>
         </div>
-        <Form>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Row>
             <Form.Group as={Col} className="mb-3" controlId="formFirstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter First Name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter First Name"
+                name="username"
+                value={data.username}
+                onChange={handleChange}
+              />
             </Form.Group>
 
-            <Form.Group as={Col} className="mb-3" controlId="formLastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last Name" />
+            <Form.Group as={Col} className="mb-3" controlId="formAddress">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your Address"
+                name="address"
+                value={data.address}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Row>
 
           <Row>
             <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+              />
 
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
@@ -39,20 +119,44 @@ function Register() {
               <Form.Control
                 type="number"
                 //pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                required
                 placeholder="Enter mobile number"
+                name="number"
+                value={data.number}
+                onChange={handleChange}
               />
 
               <Form.Text className="text-muted">
                 Enter a valid local number.
               </Form.Text>
             </Form.Group>
+            <Form.Group
+              as={Col}
+              className="mb-3"
+              controlId="formDateofBirth"
+              name="dob"
+            >
+              <Form.Label>Date Of Birth</Form.Label>
+              <DatePicker
+                showPopperArrow={true}
+                selected={value}
+                onChange={handleChange}
+                value={data.dob}
+                maxDate={new Date()}
+                customInput={<Form.Control />}
+              />
+            </Form.Group>
           </Row>
 
           <Row>
             <Form.Group as={Col} className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="passwod"
+                value={data.password}
+                onChange={handleChange}
+              />
               <Form.Text id="passwordHelpBlock" muted>
                 Your password must be 8-20 characters long, contain letters and
                 numbers, and must not contain spaces or special characters.
@@ -65,7 +169,13 @@ function Register() {
               controlId="formBasicConfirmPassword"
             >
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Retype Password" />
+              <Form.Control
+                type="password"
+                placeholder="Retype Password"
+                name="confirmPwd"
+                //value={data.password}
+                //onChange={handleChange}
+              />
             </Form.Group>
           </Row>
 
@@ -80,6 +190,6 @@ function Register() {
       </Container>
     </>
   );
-}
+};
 
 export default Register;
